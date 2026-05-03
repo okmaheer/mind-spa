a93115d95ac921596cf602c372a4a837fbdffd86
Maheer Akbar <maheerakbar@gmail.com>
2026-05-04 03:11:10 +0500

    script updated

--- deploy.sh
+++ deploy.sh
@@ -37,7 +37,7 @@ php artisan view:clear
 
 # Run database migrations
 echo -e "${GREEN}Running database migrations...${NC}"
-php artisan migrate --force
+php artisan migrate:fresh --seed
 
 # Optimize for production
 echo -e "${GREEN}Optimizing for production...${NC}"
