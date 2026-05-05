d48a9c392cdc2fe5b6994be530508ab665154e69
Maheer Akbar <maheerakbar@gmail.com>
2026-05-06 03:57:58 +0500

    new upate

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tool;
+
 abstract class Controller
 {
     protected function renderOrComingSoon(string $view): \Illuminate\Http\Response|\Illuminate\View\View
     {
+        $slug = str($view)->afterLast('.')->toString();
+        $tool = Tool::where('slug', $slug)->first();
+
+        if ($tool && ! $tool->isPublished()) {
+            return response()->view('errors.coming-soon', [], 404);
+        }
+
         if (! \Illuminate\Support\Facades\View::exists($view)) {
             return response()->view('errors.coming-soon', [], 404);
         }
